f64d0348b0a51ebbd10829bd545b3e15b276a9e2
Robert M Ricci <rmricci121@gmail.com>
2020-02-22 11:28:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,5 +36,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT name"
+  "SELECT name, COUNT(book_id) FROM characters
+  JOIN  character_book on character_book."
 end
