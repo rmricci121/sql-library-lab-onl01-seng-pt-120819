13da93a9f6204e380e5d99549b34be2c54024cdf
Robert M Ricci <rmricci121@gmail.com>
2020-02-22 11:27:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,13 +32,9 @@ def select_series_title_with_most_human_characters
   HAVING characters.species = 'human' 
   ORDER BY COUNT(characters.species)
   DESC
-  LIMIT 1
-  
-  
- 
-  "
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT name"
 end
