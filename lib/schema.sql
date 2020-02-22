18290355e2cd1b8fd3b6ca62bdee78cd5e2f6ae9
Robert M Ricci <rmricci121@gmail.com>
2020-02-22 10:23:38 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,36 +1,36 @@
 CREATE TABLE series (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
   subgenre_id INTEGER, 
 );
 
 CREATE TABLE subgenres (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
   name TEXT,
 );
 
 CREATE TABLE authors (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
   name TEXT,
 );
 
 CREATE TABLE books (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
   series_id INTEGER
 );
 
 CREATE TABLE subgenres (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
   name TEXT,
   motto TEXT,
   author_id
 );
 
 CREATE TABLE character_books (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
   book_id INTEGER,
   character_id INTEGER
 );
