4868de86c02aa7a73332f1acdacb4c130b7a75ff
Robert M Ricci <rmricci121@gmail.com>
2020-02-22 11:00:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name, subgenre "
+  "SELECT authors.name, subgenre "
 end
 
 def select_series_title_with_most_human_characters
