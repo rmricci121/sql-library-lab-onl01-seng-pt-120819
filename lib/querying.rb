d56a89c6b2bc9c53cf60235805bb386c24629c2e
Robert M Ricci <rmricci121@gmail.com>
2020-02-22 10:39:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM series 
+  "SELECT series.title, books.year FROM series 
   JOIN books ON books.series_id = series.id 
   ORDER BY books.year"
 end
