602e47fbb025d274d6e2c44f2818494fb5d8a07c
Robert M Ricci <rmricci121@gmail.com>
2020-02-22 10:47:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,7 +5,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, MAX(motto) FROM characters 
+  "SELECT name, MIN(motto) FROM characters 
   "
 end
 
