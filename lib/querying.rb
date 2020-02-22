590614dc4718b67ee2d922629084122e3bb946c2
Robert M Ricci <rmricci121@gmail.com>
2020-02-22 11:48:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,5 +38,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT name, COUNT(character_id) FROM characters
   JOIN character_books on character_books.character_id = characters.id 
+  GROUP BY characters.name
   "
 end
