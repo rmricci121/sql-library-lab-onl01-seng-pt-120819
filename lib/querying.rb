53c66985b6c339369d503eedd1a3a9f9c32cd07d
Robert M Ricci <rmricci121@gmail.com>
2020-02-22 11:13:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_series_title_with_most_human_characters
   GROUP BY characters.species
   HAVING COUNT(characters.species) = 'human'
   
-  LIMIT 1"
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
